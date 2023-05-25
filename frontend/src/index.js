36307147a8c6db6668f337c5e0a762e483e4e695
Shivam <shivam2432000@gmail.com>
2023-05-25 13:18:12 +0530

    This is redux remove from this project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,12 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ChakraProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </ChakraProvider>
   </BrowserRouter>
 );
